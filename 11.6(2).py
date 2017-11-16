7116bfa39e253b58a1a05b215764babd4bf33370
1617193150 <33270530+1617193150@users.noreply.github.com>
2017-11-16 14:22:57 +0800

    Add files via upload

---
+++ 11.6(2).py
@@ -0,0 +1,8 @@
+Python 2.7.13 (v2.7.13:a06454b1afa1, Dec 17 2016, 20:42:59) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> headers = { 'User-Agent' : 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'  ,
+	    'Referer':'http://www.zhihu.com/articles' }
+>>> 
+>>> 'Referer':'http://www.zhihu.com/articles' }
+SyntaxError: invalid syntax
+>>> 
